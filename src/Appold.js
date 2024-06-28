31c9a3acec7fb8a533514c12270e8fbe6e090e5b
toguvr <augustotf93@gmail.com>
2024-06-28 15:35:13 -0300

    fix queues services

--- src/Appold.js
+++ src/Appold.js
@@ -7,15 +7,15 @@ function App() {
   const user_id = localStorage.getItem('user_id');
   const port = localStorage.getItem('port');
   const [message, setMessage] = useState('');
-  const url = 'https://api-dev.hubees.com.br/socket';
+  const url = 'https://api-dev.hubees.com.br';
   // const url = `http://localhost:${port}`;
   // const url = `http://localhost:3333?user_id=${user_id}`;
   const token =
     'eyJhbGciOiJSUzI1NiIsImtpZCI6IjNhM2JkODk4ZGE1MGE4OWViOWUxY2YwYjdhN2VmZTM1OTNkNDEwNjgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYXBwbmV0cGFya2RldiIsImF1ZCI6ImFwcG5ldHBhcmtkZXYiLCJhdXRoX3RpbWUiOjE3MDIzOTYxOTUsInVzZXJfaWQiOiJNQWlXSzJ2VGNLVU5xTnpwcWkwaFFLNUdqSzcyIiwic3ViIjoiTUFpV0sydlRjS1VOcU56cHFpMGhRSzVHaks3MiIsImlhdCI6MTcwMjM5NjE5NSwiZXhwIjoxNzAyMzk5Nzk1LCJlbWFpbCI6ImJlbGxhcXVhcmFudGErYmVlbW92aW5nOEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiYmVsbGFxdWFyYW50YStiZWVtb3Zpbmc4QGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.W47SE6_juVqt1SbC1Qywy9opGCUu41Rnt0wThRmAapygbFYGv6T5CcehIJihXTsXC6wWh6nkuki89VWmjaCJvi9NY4J9qDFtwJURIRngufPwKw5bhFCw1AlkmLfR63TFaFoIPVjhlvaSysCxQu3JDtW5mHOQ_owCNca5WGSGBlheTjP3Tn2qnoBWmTd4zATIo3yd9nEr8l8aWHJT5oF8im78VdGmZuV0wqMV0-36JS4-FCfctmaR9GIn4hA-yajJY4XGg9F_EcHpyoma4wqYIex98-s3cxGoGS9y42uUqzK7bdwb6SZNRn9CplXXELU88OtWxnktQwJ7ZeuTy9LWBQ';
   const socket = useMemo(() => {
     return io(url, {
-      autoConnect: true,
-      transports: ['websocket', 'polling'],
+      path: '/socket',
+      transports: ['websocket'],
       auth: {
         authorization: 'Bearer ' + token,
       },
@@ -26,6 +26,9 @@ function App() {
       console.log('recebido', data);
       setMessage(JSON.stringify(data));
     });
+
+    socket.on('error', (err) => console.log('error', err));
+    socket.on('connect', () => console.log('connect'));
   }, [socket]);
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
+import { BrowserRouter } from 'react-router-dom';
+
+import { msalConfig } from './authConfig.js';
+import App from './App.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
+
+/**
+ * MSAL should be instantiated outside of the component tree to prevent it from being re-instantiated on re-renders.
+ * For more, visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+ */
+const msalInstance = new PublicClientApplication(msalConfig);
+
+// Default to using the first account if no account is active on page load
+if (
+  !msalInstance.getActiveAccount() &&
+  msalInstance.getAllAccounts().length > 0
+) {
+  // Account selection logic is app dependent. Adjust as needed for different use cases.
+  msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+}
+
+msalInstance.addEventCallback((event) => {
+  if (
+    (event.eventType === EventType.LOGIN_SUCCESS ||
+      event.eventType === EventType.ACQUIRE_TOKEN_SUCCESS ||
+      event.eventType === EventType.SSO_SILENT_SUCCESS) &&
+    event.payload.account
+  ) {
+    msalInstance.setActiveAccount(event.payload.account);
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App instance={msalInstance} />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
