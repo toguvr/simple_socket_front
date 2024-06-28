839efadcce20d8df84139ac497ff83271588166e
toguvr <augustotf93@gmail.com>
2024-06-28 15:57:03 -0300

    socket

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,67 @@
-import { useEffect } from 'react';
-import { Routes, Route } from "react-router-dom";
-import { MsalProvider, useMsal } from '@azure/msal-react';
-import { EventType } from '@azure/msal-browser';
+import logo from './logo.svg';
+import './App.css';
+import { io } from 'socket.io-client';
+import { useEffect, useMemo, useState } from 'react';
 
-import { PageLayout } from './components/PageLayout';
-import { TodoList } from './pages/TodoList';
-import { Home } from './pages/Home';
-import { b2cPolicies, protectedResources } from './authConfig';
-import { compareIssuingPolicy } from './utils/claimUtils';
+function App() {
+  const user_id = localStorage.getItem('user_id');
+  const port = localStorage.getItem('port');
+  const [message, setMessage] = useState('');
+  const [sendMsg, setsendMsg] = useState('');
+//   const url = 'https://api-dev.hubees.com.br';
+  // const url = `http://localhost:${port}`;
+  const url = `http://localhost:3333?user_id=${user_id}`;
+  const token =
+    'eyJhbGciOiJSUzI1NiIsImtpZCI6ImYwOGU2ZTNmNzg4ZDYwMTk0MDA1ZGJiYzE5NDc0YmY5Mjg5ZDM5ZWEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYXBwbmV0cGFya2RldiIsImF1ZCI6ImFwcG5ldHBhcmtkZXYiLCJhdXRoX3RpbWUiOjE3MTgzNzA4MzYsInVzZXJfaWQiOiJ4dm1sQlI2S0FoUnpWQTloTDZrZk53ZmFBTTcyIiwic3ViIjoieHZtbEJSNktBaFJ6VkE5aEw2a2ZOd2ZhQU03MiIsImlhdCI6MTcxOTU5OTU1NSwiZXhwIjoxNzE5NjAzMTU1LCJlbWFpbCI6ImF1Z3VzdG90ZjkzK3NpbmdsZUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiYXVndXN0b3RmOTMrc2luZ2xlQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.qMG60ynsngBIIxdfX_WJjGwCQa5M1HUpPsNMXP65g0iAXm3ryHRAXN-Ebv5aT2eDp0ZnhBCxUb1D58knMm352qXoknFFZsPvOsPBAMhHCMbPISQkfn142MPJc74SABhLjjGAhgiIzZdZN4KKTlTtEwAuYHDOZeDww3j3KFqX4FAGlUCw61qujUT5GqgVKNsEkzWamsGDZoYUbOysJ0mcCO8gf1xKhP08wF-KfTLwNARJd8mxYaTOV2cqhJqqWhdfzNlv2omh2yu7dxv9DSM3DfGcKZtajelq3QCqs3GrzQ1X86xBb7Nds-6N8RgqJY8Pd1bbqrlRdQxFIbYu-EnSpA';
+  const socket = useMemo(() => {
+    return io(url, {
+      path: '/socket',
+      transports: ['websocket'],
+      auth: {
+        authorization: 'Bearer ' + token,
+      },
+    });
+  }, []);
+  useEffect(() => {
+    socket.on('private_message', (data) => {
+      console.log('recebido', data);
+      setMessage(data.msg);
+    });
 
-import './styles/App.css';
+    socket.on('error', (err) => console.log('error', err));
+    socket.on('connect', () => console.log('connect'));
+  }, [socket]);
+  const sendMsgClicked = () => {
+    console.log({
+      anotherUserId: user_id == '1' ? '2' : '1',
+      msg: sendMsg,
+    });
 
-const Pages = () => {
-    /**
-     * useMsal is hook that returns the PublicClientApplication instance,
-     * an array of all accounts currently signed in and an inProgress value
-     * that tells you what msal is currently doing. For more, visit:
-     * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/hooks.md
-     */
-    const { instance } = useMsal();
-    useEffect(() => {
-        const callbackId = instance.addEventCallback((event) => {
-            if (
-                (event.eventType === EventType.LOGIN_SUCCESS || event.eventType === EventType.ACQUIRE_TOKEN_SUCCESS) &&
-                event.payload.account
-            ) {
-                /**
-                 * For the purpose of setting an active account for UI update, we want to consider only the auth
-                 * response resulting from SUSI flow. "tfp" claim in the id token tells us the policy (NOTE: legacy
-                 * policies may use "acr" instead of "tfp"). To learn more about B2C tokens, visit:
-                 * https://docs.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview
-                 */
-                if (compareIssuingPolicy(event.payload.idTokenClaims, b2cPolicies.names.editProfile)) {
-                    // retrieve the account from initial sing-in to the app
-                    const originalSignInAccount = instance
-                        .getAllAccounts()
-                        .find(
-                            (account) =>
-                                account.idTokenClaims.oid === event.payload.idTokenClaims.oid &&
-                                account.idTokenClaims.sub === event.payload.idTokenClaims.sub && 
-                                compareIssuingPolicy(account.idTokenClaims, b2cPolicies.names.signUpSignIn)        
-                        );
+    socket.emit('private_message', {
+      anotherUserId: user_id == '1' ? '2' : '1',
+      msg: sendMsg,
+    });
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
 
-                    let signUpSignInFlowRequest = {
-                        authority: b2cPolicies.authorities.signUpSignIn.authority,
-                        account: originalSignInAccount,
-                    };
+        <label htmlFor="">MENSAGEM PARA O OUTRO</label>
 
-                    // silently login again with the signUpSignIn policy
-                    instance.ssoSilent(signUpSignInFlowRequest);
-                }
-
-                /**
-                 * Below we are checking if the user is returning from the reset password flow.
-                 * If so, we will ask the user to reauthenticate with their new password.
-                 * If you do not want this behavior and prefer your users to stay signed in instead,
-                 * you can replace the code below with the same pattern used for handling the return from
-                 * profile edit flow
-                 */
-                if (compareIssuingPolicy(event.payload.idTokenClaims, b2cPolicies.names.forgotPassword)) {
-                    let signUpSignInFlowRequest = {
-                        authority: b2cPolicies.authorities.signUpSignIn.authority,
-                        scopes: [
-                            ...protectedResources.apiTodoList.scopes.read,
-                            ...protectedResources.apiTodoList.scopes.write,
-                        ],
-                    };
-                    instance.loginRedirect(signUpSignInFlowRequest);
-                }
-            }
-
-            if (event.eventType === EventType.LOGIN_FAILURE) {
-                // Check for forgot password error
-                // Learn more about AAD error codes at https://docs.microsoft.com/en-us/azure/active-directory/develop/reference-aadsts-error-codes
-                if (event.error && event.error.errorMessage.includes('AADB2C90118')) {
-                    const resetPasswordRequest = {
-                        authority: b2cPolicies.authorities.forgotPassword.authority,
-                        scopes: [],
-                    };
-                    instance.loginRedirect(resetPasswordRequest);
-                }
-            }
-        });
-
-        return () => {
-            if (callbackId) {
-                instance.removeEventCallback(callbackId);
-            }
-        };
-        // eslint-disable-next-line
-    }, [instance]);
-
-    return (
-        <Routes>
-            <Route path="/todolist" element={<TodoList />} />
-            <Route path="/" element={<Home />} />
-        </Routes>
-    );
-};
-
-/**
- * msal-react is built on the React context API and all parts of your app that require authentication must be
- * wrapped in the MsalProvider component. You will first need to initialize an instance of PublicClientApplication
- * then pass this to MsalProvider as a prop. All components underneath MsalProvider will have access to the
- * PublicClientApplication instance via context as well as all hooks and components provided by msal-react. For more, visit:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
- */
-const App = ({ instance }) => {
-    return (
-        <MsalProvider instance={instance}>
-            <PageLayout>
-                <Pages />
-            </PageLayout>
-        </MsalProvider>
-    );
-};
+        <input
+          type="text"
+          onChange={(e) => setsendMsg(e.target.value)}
+          value={sendMsg}
+        />
+        <button onClick={sendMsgClicked}>send</button>
+        <p>
+          User: {user_id} | port: {port}
+        </p>
+        <p>novo: {message}</p>
+      </header>
+    </div>
+  );
+}
 
 export default App;
